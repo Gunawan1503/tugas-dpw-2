3dd7bd9b7cae7f44abbfd533f2f6ee0653040266
Gunawan1503 <70994976+Gunawan1503@users.noreply.github.com>
2020-10-29 06:22:17 +0700

    Update_ikon

--- system/resources/views/home.blade.php
+++ system/resources/views/home.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
     <script src="https://kit.fontawesome.com/d0ee1fa2b0.js" crossorigin="anonymous"></script>
+    <link rel="icon" type="image/png" sizes="16x16" href="{{ url('public') }}/assets/images/favicon.png">
     <title>WS Shop</title>
   </head>
   <body>
